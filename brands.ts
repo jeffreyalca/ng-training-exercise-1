d630020fbc2bee7eae4228dcc8604dbb1cb48886
Jeffrey Alca <jeffrey.alca@essilor.com>
2019-11-28 11:30:50 +0800

    added test data

---
+++ brands.ts
@@ -0,0 +1,146 @@
+let brands = [
+    {
+      "id": 38,
+      "name": "BLOOM"
+    },
+    {
+      "id": 21,
+      "name": "COACH"
+    },
+    {
+      "id": 15,
+      "name": "CONVERSE"
+    },
+    {
+      "id": 16,
+      "name": "COSTA"
+    },
+    {
+      "id": 48,
+      "name": "DANNY GOKEY"
+    },
+    {
+      "id": 52,
+      "name": "EMPORIO ARMANI"
+    },
+    {
+      "id": 36,
+      "name": "EVATIK"
+    },
+    {
+      "id": 23,
+      "name": "FYSH"
+    },
+    {
+      "id": 14,
+      "name": "GARGOYLES"
+    },
+    {
+      "id": 13,
+      "name": "GUCCI"
+    },
+    {
+      "id": 12,
+      "name": "GUESS"
+    },
+    {
+      "id": 53,
+      "name": "GUESS BY MARCIANO"
+    },
+    {
+      "id": 8,
+      "name": "GWEN STEFANI GX"
+    },
+    {
+      "id": 37,
+      "name": "HACKETT"
+    },
+    {
+      "id": 55,
+      "name": "HELIUM PARIS"
+    },
+    {
+      "id": 33,
+      "name": "JOHN VARVATOS"
+    },
+    {
+      "id": 11,
+      "name": "KENNETH COLE"
+    },
+    {
+      "id": 51,
+      "name": "KENZO"
+    },
+    {
+      "id": 24,
+      "name": "KLIIK"
+    },
+    {
+      "id": 9,
+      "name": "L.A.M.B."
+    },
+    {
+      "id": 57,
+      "name": "LIGHTEC"
+    },
+    {
+      "id": 60,
+      "name": "LULU GUINESS"
+    },
+    {
+      "id": 22,
+      "name": "MICHAEL KORS"
+    },
+    {
+      "id": 31,
+      "name": "OAKLEY"
+    },
+    {
+      "id": 39,
+      "name": "PRADA"
+    },
+    {
+      "id": 28,
+      "name": "PUMA"
+    },
+    {
+      "id": 3,
+      "name": "RAY BAN"
+    },
+    {
+      "id": 4,
+      "name": "RISERVA"
+    },
+    {
+      "id": 7,
+      "name": "TED BAKER"
+    },
+    {
+      "id": 30,
+      "name": "TIMBERLAND"
+    },
+    {
+      "id": 34,
+      "name": "TITANFLEX"
+    },
+    {
+      "id": 29,
+      "name": "TOM FORD"
+    },
+    {
+      "id": 35,
+      "name": "TURA"
+    },
+    {
+      "id": 58,
+      "name": "VERSACE"
+    },
+    {
+      "id": 10,
+      "name": "VICIOUS VISION"
+    },
+    {
+      "id": 27,
+      "name": "VISION SOURCE SMART"
+    }
+  ];
